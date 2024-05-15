84eae8d1337abee9f9166673727f6194f0686c78
nhannttdevvn <nhanntt.devvn@gmail.com>
2024-05-16 02:35:51 +0700

    code link các trang

--- Online_Shopping/app/views.py
+++ Online_Shopping/app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'app/home.html')
+    products= Product.objects.all()
+    context={'products':products}
+    return render(request,'app/home.html',context)
+
+def cart(request):
+    context={}
+    return render(request,'app/cart.html',context)
+
+def checkout(request):
+    context={}
+    return render(request,'app/checkout.html',context)
+
