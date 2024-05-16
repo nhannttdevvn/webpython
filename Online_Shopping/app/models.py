536fb52586b9057b75d9879d553b69f217660f96
nhannttdevvn <nhanntt.devvn@gmail.com>
2024-05-16 23:50:06 +0700

    code hiển thị sp

--- Online_Shopping/app/models.py
+++ Online_Shopping/app/models.py
@@ -13,12 +13,21 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True,blank=False)
+    image  = models.ImageField(null=True,blank=True)
     
     def __str__(self):
         return self.name
-class Oder(models.Model):
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+    
+class Order(models.Model):
     Customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
-    date_oder = models.DateTimeField(auto_now_add=True)
+    date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False,null=True,blank=False)
     transaction_id = models.CharField(max_length=200,null=True)
 
@@ -26,18 +35,18 @@ class Oder(models.Model):
         return str(self.id)
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
-    order = models.ForeignKey(Oder,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     quality = models.IntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
-    order = models.ForeignKey(Oder,on_delete=models.SET_NULL,blank=True,null=True)
-    address = models.CharField(max_length=200,null=True)
-    cyti = models.CharField(max_length=200,null=True)
-    state = models.CharField(max_length=200,null=True)
-    date_added = models.CharField(max_length=200,null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)  # Sửa lại tên trường city
+    state = models.CharField(max_length=200, null=True)
+    date_added = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.address
