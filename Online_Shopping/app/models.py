ff9f4353dee15d644616fe4c46089b322cb374b8
nhannttdevvn <nhanntt.devvn@gmail.com>
2024-05-24 21:35:31 +0700

    fix: style

--- Online_Shopping/app/models.py
+++ Online_Shopping/app/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 #Change forms register Django
+
+
+class Catelogy(models.Model): 
+    sub_catelory = models.ForeignKey('self',on_delete=models.CASCADE, related_name='sub_catelories',null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.name
+    
 class CreateUserForm(UserCreationForm): #KE THUA CUA django
      class Meta:
         model = User
         fields = ['username','email','first_name','last_name','password1','password2']
-
-# class Customer(models.Model):
-#     user = models.OneToOneField(User,on_delete=models.SET_NULL,null=True,blank=False)
-#     name = models.CharField(max_length=200,null=True)
-#     email = models.CharField(max_length=200,null=True)
-
-#     def __str__(self):
-#         return self.name
-
 class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
