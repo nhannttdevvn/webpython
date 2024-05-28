5bb7454d2ed763e369050b2fd417af781f8f5497
nhannttdevvn <nhanntt.devvn@gmail.com>
2024-05-28 12:38:06 +0700

    fix giaodien

--- Online_Shopping/app/views.py
+++ Online_Shopping/app/views.py
@@ -17,10 +17,14 @@ def search(request):
         order, created = Order.objects.get_or_create(customer=customer, complete = False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else:
         items = []
         order={'get_cart_items':0, 'get_cart_total':0}
         cartItems = order['get_cart_items'] 
+        user_not_login = "show"
+        user_login = "hidden"
 
 
     products= Product.objects.all()
